ab9b2a20496b1132fb6340867ab7acbbb93d0914
walshja9 <walshja9@gmail.com>
2026-09-09 19:01:52 -0400

    Refresh ratings iframe for postseason and defensive depth release

--- tests/test_shopify_theme.py
+++ tests/test_shopify_theme.py
@@ -177,7 +177,7 @@ class ShopifyThemeTests(unittest.TestCase):
             setting = next(item for item in schema["settings"] if item["id"] == setting_id)
             self.assertNotIn("default", setting, setting_id)
         settings = template["sections"]["main"]["settings"]
-        self.assertEqual("https://walshja9.github.io/Postgame_Outlet/index.html?release=8305414", settings["ratings_url"])
+        self.assertEqual("https://walshja9.github.io/Postgame_Outlet/index.html?release=9a6cfd8", settings["ratings_url"])
         self.assertEqual("/pages/methodology", settings["methodology_link"])
         self.assertEqual("/pages/accountability", settings["accountability_link"])
         self.assertEqual("/blogs/poweratings", settings["archive_link"])
